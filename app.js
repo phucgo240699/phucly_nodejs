046202f9c3003b02e4919d9c866924989250f384
phucgo240699 <phucgo240699@gmail.com>
2020-02-03 12:31:13 +0700

    Update UI

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require('express')
+var path = require('path')
 var hbs = require('hbs')
 
 var app = express()
 
-app.listen(process.env.PORT || 3001)
+
+const publicDirectoryPath = path.join(__dirname, '/public')
+
+console.log(publicDirectoryPath)
+app.listen(process.env.PORT || 3000)
 
 app.set('view engine', 'hbs')
-app.set('views','./view')
+app.set('views','./views')
+
+app.use(express.static(publicDirectoryPath))
 
 
 app.get('/', (request, response) => {
